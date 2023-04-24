e7e921f7afa72cd03c77890e5e9b9d148e02d5e0
and265 <darwin.17.ar@gmail.com>
2023-04-23 20:35:53 -0400

    Construcción subclase Moneda1500.

--- Tarea1/src/Moneda1500.java
+++ Tarea1/src/Moneda1500.java
@@ -1,2 +1,9 @@
-public class Moneda1500 {
+class Moneda1500 extends Moneda{
+    public Moneda1500(){
+        super();
+        super.valor = 1500;
+    }
+    public int getValor(){
+        return super.valor;
+    }
 }
