c4767f539d1046154931d0bbc5fb2d5e8674acfe
and265 <darwin.17.ar@gmail.com>
2023-04-23 20:09:15 -0400

    Deposito para Productos en desarrollo.

--- Tarea1/src/Deposito.java
+++ Tarea1/src/Deposito.java
@@ -1,2 +1,25 @@
-public class Deposito {
+import java.util.ArrayList;
+
+class Deposito{
+    private ArrayList<Bebida> bebidas;
+
+    public Deposito(){
+        bebidas = new ArrayList<Bebida>();
+    }
+
+    public void addBebida(Bebida beb){
+        bebidas.add(beb);
+    }
+
+    public Bebida getBebida(){
+        if (bebidas.size() == 0){
+            return null;
+        }
+        else{
+            Bebida ret = bebidas.get(0);
+            bebidas.remove(0);
+            return ret;
+        }
+    }
 }
+
