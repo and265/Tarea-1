751ddd95e416005259114d7b5183773c58a4b185
and265 <darwin.17.ar@gmail.com>
2023-04-23 20:50:05 -0400

    Construcción de excepción para indicar que el pago ingresado no es válido.

--- Tarea1/src/PagoIncorrectoException.java
+++ Tarea1/src/PagoIncorrectoException.java
@@ -1,2 +1,12 @@
-public class PagoIncorrectoException {
+/**Excepcion que indica que el pago ingresado no es valido.
+ */
+public class PagoIncorrectoException extends Exception {
+
+    /** Constructor, recibe un String para generar un mensaje de error.
+     * @param errorMessage String
+     */
+    public PagoIncorrectoException(String errorMessage) {
+
+        super(errorMessage);
+    }
 }
