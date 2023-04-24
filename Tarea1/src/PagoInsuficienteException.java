d2964c251903af84cd3a6455289a605bc6359af1
and265 <darwin.17.ar@gmail.com>
2023-04-23 20:51:39 -0400

    Construcción de excepción para indicar que el pago ingresado no es suficiente.

--- Tarea1/src/PagoInsuficienteException.java
+++ Tarea1/src/PagoInsuficienteException.java
@@ -1,2 +1,12 @@
-public class PagoInsuficienteException {
+/**Excepcion que indica que el pago ingresado no es suficiente.
+ */
+public class PagoInsuficienteException extends Exception {
+
+    /** Constructor, recibe un String para generar un mensaje de error.
+     * @param errorMessage String
+     */
+    public PagoInsuficienteException(String errorMessage) {
+
+        super(errorMessage);
+    }
 }
