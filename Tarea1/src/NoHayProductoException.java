cf65d764a3a04b8124a5cf65262ffa7ff9b69423
and265 <darwin.17.ar@gmail.com>
2023-04-23 20:47:43 -0400

    Construcción de excepción para indicar la no disponibilidad de un producto.

--- Tarea1/src/NoHayProductoException.java
+++ Tarea1/src/NoHayProductoException.java
@@ -1,2 +1,12 @@
-public class NoHayProductoException {
+/**Excepcion que indica que el producto solicitado no está disponible.
+ */
+public class NoHayProductoException extends Exception {
+
+    /** Constructor, recibe un String para generar un mensaje de error.
+     * @param errorMessage String
+     */
+    public NoHayProductoException(String errorMessage) {
+
+        super(errorMessage);
+    }
 }
