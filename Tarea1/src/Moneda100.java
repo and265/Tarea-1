ebcc59970196b1fca5b603e569e9871102765086
and265 <darwin.17.ar@gmail.com>
2023-04-23 20:32:00 -0400

    Construcción subclase Moneda100.

--- Tarea1/src/Moneda100.java
+++ Tarea1/src/Moneda100.java
@@ -1,2 +1,9 @@
-public class Moneda100 {
+class Moneda100 extends Moneda{
+    public Moneda100(){
+        super();
+        super.valor = 100;
+    }
+    public int getValor(){
+        return super.valor;
+    }
 }
