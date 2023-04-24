053d98a262e413e7894817bf53bbbe877c66888c
and265 <darwin.17.ar@gmail.com>
2023-04-23 20:17:07 -0400

    Construcción de déposito de monedas.

--- Tarea1/src/DepositoM.java
+++ Tarea1/src/DepositoM.java
@@ -1,2 +1,24 @@
-public class DepositoM {
+import java.util.ArrayList;
+
+class DepositoM{
+    private ArrayList<Moneda> monedas;
+    public DepositoM(){
+        monedas = new ArrayList<Moneda>();
+    }
+
+    public void addMoneda(Moneda mon){
+        monedas.add(mon);
+    }
+
+    public Moneda getMoneda(){
+        if (monedas.size() == 0){
+            return null;
+        }
+        else{
+            Moneda ret = monedas.get(0);
+            monedas.remove(0);
+            return ret;
+        }
+    }
+
 }
