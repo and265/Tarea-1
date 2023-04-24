4c7344cf58d7a8e26975ae9e91ec763e98d5aa57
and265 <darwin.17.ar@gmail.com>
2023-04-23 20:42:56 -0400

    Comprador en desarrollo.

--- Tarea1/src/Comprador.java
+++ Tarea1/src/Comprador.java
@@ -1,2 +1,23 @@
-public class Comprador {
+class Comprador{
+    private String sonido;
+    private int vuelto;
+
+    public Comprador(Moneda m, int bebidaId, Expendedor ex){
+        vuelto = 0;
+
+        Bebida beb = ex.comprarBebida(m, bebidaId);
+        if(beb != null) {
+            sonido = beb.beber();
+        }
+        Moneda aux = ex.getVuelto();
+        while (aux != null){
+            vuelto += aux.getValor();
+            aux = ex.getVuelto();
+        }
+    }
+
+    public int cuantoVuelto() {return vuelto;}
+
+    public String queBebiste() {return sonido;}
+
 }
