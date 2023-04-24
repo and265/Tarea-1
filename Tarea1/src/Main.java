ac053b59fdef431fabdb11f023571e65c0694cbe
and265 <darwin.17.ar@gmail.com>
2023-04-23 20:56:34 -0400

    Construcción de clase Main sujeta a revisión.

--- Tarea1/src/Main.java
+++ Tarea1/src/Main.java
@@ -1,6 +1,34 @@
-
 public class Main {
     public static void main(String[] args) {
+
+        Expendedor exp = new Expendedor(0, 500);
+        Moneda m = null;
+        Comprador c = null;
+        m = new Moneda500();
+        System.out.println(c.queBebiste()+", "+c.cuantoVuelto());
+
+        try {
+
+            c = new Comprador(m, 1, exp);
+            exp.comprarBebida(m, 1);
+        } catch (PagoIncorrectoException e) {
+
+            e.printStackTrace(System.out);
+
+            System.out.println("El pago ingresado no es correcto");
+
+        } catch (PagoInsuficienteException e) {
+
+            e.printStackTrace(System.out);
+
+            System.out.println("El pago ingresado no es suficiente");
+
+        } catch (NoHayProductoException e) {
+
+            System.out.println("El producto solicitado no se encuentra disponible");
+
+        }
+
 
     }
 }
