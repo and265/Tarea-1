814f348afc9d7d19b3853fcdcfc65be1b6070c84
and265 <darwin.17.ar@gmail.com>
2023-04-23 20:30:02 -0400

    Construcción clase Moneda.

--- Tarea1/src/Moneda.java
+++ Tarea1/src/Moneda.java
@@ -1,2 +1,14 @@
-public class Moneda {
+abstract class Moneda{
+
+    protected int valor;
+    protected int serie;
+
+    public Moneda(){
+    }
+    public Moneda getSerie(){
+        return this;
+    }
+
+    public abstract int getValor();
+
 }
