84cd21a9c318a8f986e27e6d7d464ec84af2f076
and265 <darwin.17.ar@gmail.com>
2023-04-23 20:21:34 -0400

    Construcción clase Bebida.

--- Tarea1/src/Bebida.java
+++ Tarea1/src/Bebida.java
@@ -1,2 +1,13 @@
-public class Bebida {
+abstract class Bebida{
+    private int serie;
+
+    public Bebida(int s){
+        serie = s;
+    }
+
+    public int getSerie(){
+        return serie;
+    }
+
+    public abstract String beber();
 }
