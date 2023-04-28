336582a46625093876324812e549dd40cae0acc5
and265 <131011527+and265@users.noreply.github.com>
2023-04-28 18:47:29 -0400

    Actualización subclase Sprite
    Se añade documentación al código.

--- Tarea1/src/Sprite.java
+++ Tarea1/src/Sprite.java
@@ -1,9 +1,23 @@
-class Sprite extends Bebida{
+/** 
+ * Una bebida de tipo Sprite
+ * @author Darwin Albornoz
+ * @author Alex Blanchard
+ * @version versión 1, 28 de abril de 2023
+ */
 
+class Sprite extends Bebida{
+    
+    /**Metodo constructor clase Sprite.
+    * @param s primero int
+    */
     public Sprite(int s){
         super(s);
     }
-    public String beber(){
+    
+    /**Metodo consumir
+    * return un string con el nombre del producto "sprite".
+    */
+    public String consumir(){
         return "sprite";
     }
 }
