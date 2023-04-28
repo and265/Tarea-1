9750b7c6f4e28996fe08bdf30baf4bfce3fac029
and265 <131011527+and265@users.noreply.github.com>
2023-04-28 18:30:57 -0400

    Actualización clase Bebida
    Se añade documentación al código.

--- Tarea1/src/Bebida.java
+++ Tarea1/src/Bebida.java
@@ -1,5 +1,15 @@
+/** 
+ * Representacion de una bebida que hereda atributos y metodos de Produto para pasarlos a sus subclases CocaCola y Srpite.
+ * @author Darwin Albornoz
+ * @author Alex Blanchard
+ * @version versión 1, 28 de abril de 2023
+ */
+
 abstract class Bebida extends Producto{
 
+    /** Metodo constructor clase Bebida
+    *@param serie primero int 
+    */
     public Bebida(int serie){
         super(serie);
 
