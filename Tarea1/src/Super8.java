219b52a936b7e7126a5d1b1cba5f09055cefb405
and265 <131011527+and265@users.noreply.github.com>
2023-04-28 16:47:37 -0400

    Actualizacion subclase Super8
    Asignación de constructor y método consumir.

--- Tarea1/src/Super8.java
+++ Tarea1/src/Super8.java
@@ -1,2 +1,10 @@
-public class Super8 {
+public class Super8 extends Dulce {
+
+    public Super8(int s){
+        super(s);
+    }
+
+    public String consumir(){
+        return "super8";
+    }
 }
