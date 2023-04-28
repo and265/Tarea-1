44f6f620d7f6610a7680fe0b7eaa6ce3465ca2a2
and265 <131011527+and265@users.noreply.github.com>
2023-04-28 16:37:25 -0400

    Actualización clase Main
    Se le asigna una tercera entrada a objeto de la clase Comprador para el precio de dulces.

--- Tarea1/src/Main.java
+++ Tarea1/src/Main.java
@@ -1,15 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 
-        Expendedor exp = new Expendedor(2, 500);
+        Expendedor exp = new Expendedor(4, 500, 500);
         Moneda m = null;
 
         m = new Moneda1000();
 
         try {
 
-            Comprador c = new Comprador(m, 1, exp);
-            System.out.println(c.queBebiste());
+            Comprador c = new Comprador(m, 4, exp);
+            System.out.println(c.queConsumiste()+ ", " +c.cuantoVuelto());
             //exp.comprarBebida(m, 1); no es necesario comprar bebiba manualmente,
             //                         esta implemetado en la clase Comprador.
 
@@ -17,17 +17,19 @@ public class Main {
 
             e.printStackTrace(System.out);
 
-            System.out.println("El pago ingresado no es correcto");
+            System.out.println("El pago ingresado no es correcto.");
 
         } catch (PagoInsuficienteException e) {
 
             e.printStackTrace(System.out);
 
-            System.out.println("El pago ingresado no es suficiente");
+            System.out.println("El pago ingresado no es suficiente.");
 
         } catch (NoHayProductoException e) {
 
-            System.out.println("El producto solicitado no se encuentra disponible");
+            e.printStackTrace(System.out);
+
+            System.out.println("El producto solicitado no se encuentra disponible.");
 
         }
 
