90f78b4e8a6146db4f4a2490a326e97fcc36b07c
and265 <131011527+and265@users.noreply.github.com>
2023-04-28 18:02:55 -0400

    Actualización clase Comprador
    Se añade documentación al código.

--- Tarea1/src/Comprador.java
+++ Tarea1/src/Comprador.java
@@ -1,11 +1,28 @@
+/** 
+ * Clase que simula a un comprador inresando dinero y seleccionando un producto en un expendedor.
+ * @author Nombre Darwin Albornoz
+ * @author Nombre Alex Blanchard
+ * @version versión 1, 28 de abril de 2023
+ * @see PagoIncorrectoException
+ */
+
 class Comprador{
     private String sonido;
     private int vuelto;
+    
+    /** Metodo constructor clase Comprador
+    * @param m primero Moneda 
+    * @param productoId segundo int
+    * @param ex tercero Expendedor
+    * @throws  PagoIncorrectoException  puede lanzar esta excepción si la moneda ingresada no es valida (null).
+    */
 
     public Comprador(Moneda m, int productoId, Expendedor ex) throws PagoIncorrectoException, PagoInsuficienteException, NoHayProductoException {
         vuelto = 0;
-        if( m ==  null){
-            throw new PagoIncorrectoException("Se intento comprar sin moneda");
+        sonido = "null";
+        
+        if( m ==  null){ 
+            throw new PagoIncorrectoException("Se intento comprar sin moneda"); 
         }
         Producto p = ex.comprarProducto(m, productoId);
         if(p != null) {
@@ -17,9 +34,11 @@ class Comprador{
             aux = ex.getVuelto();
         }
     }
-
+    
+    // Metodo cuantoVuelto, retorna el valor del vuelto retirado por el comprador.
     public int cuantoVuelto() {return vuelto;}
-
+    
+    // Metodo queConsumiste, retorna un string indicando el producto adquirido por el comprador desde el expendedor.
     public String queConsumiste() {return sonido;}
 
 }
